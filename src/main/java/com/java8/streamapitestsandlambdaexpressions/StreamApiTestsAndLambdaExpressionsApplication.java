7200d5915c5ac5ba3413958bddd45a33fdd991c2
Yogesh Bala <Yogeshbala024@gmail.com>
2023-02-21 20:30:06 -0600

    Initial commit

---
+++ src/main/java/com/java8/streamapitestsandlambdaexpressions/StreamApiTestsAndLambdaExpressionsApplication.java
@@ -0,0 +1,13 @@
+package com.java8.streamapitestsandlambdaexpressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiTestsAndLambdaExpressionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiTestsAndLambdaExpressionsApplication.class, args);
+	}
+
+}
